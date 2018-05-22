8ef98893c8826c6dde5918dc0ab4fecfff37377d
Rodrigo <rodrigodot42@gmail.com>
2018-05-21 21:40:06 -0300

    Added the first Axios request

--- app4/src/components/itens-list.js
+++ app4/src/components/itens-list.js
@@ -5,12 +5,23 @@ import {
   View
 } from 'react-native';
 
+import Axios from 'axios';
 import Item from './item.js';
 
  class ItensList extends Component {
 
   constructor(props) {
     super(props);
+  }
+
+  componentWillMount() {
+    Axios.get('http://faus.com.br/recursos/c/dmairr/api/itens.html')
+      .then((res) => {
+        console.log(res);
+      })
+      .catch(() => {
+        console.log('Erro ao recuperar os dados.');
+      });
   }
 
   render() {
